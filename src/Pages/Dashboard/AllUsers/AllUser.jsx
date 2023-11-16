4419e97028567ba0c04d4c8e46b64e51fdcf2bab
sazin222 <mahadiislamsazin.com>
2023-11-16 14:47:53 +0600

    token store in localstorage and send the token backend

--- src/Pages/Dashboard/AllUsers/AllUser.jsx
+++ src/Pages/Dashboard/AllUsers/AllUser.jsx
@@ -1,15 +1,65 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { FaTrash, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUser = () => {
     const axiosSecure = useAxiosSecure()
-    const {data: users=[],}= useQuery({
+    const {data: users=[], refetch}= useQuery({
         queryKey:['users'],
         queryFn: async ()=>{
-          const res = await axiosSecure.get('/users')
+          const res = await axiosSecure.get('/users',{
+            headers:{
+                authorization: `Bearer${localStorage.getItem('access-token')}`
+            }
+          })
           return res.data
         }
     })
+
+    const handelMakeAdmin = (user)=>{
+        axiosSecure.patch(`/users/admin/${user._id}`)
+        .then(res=>{
+            console.log(res.data);
+            if(res.data.modifiedCount>0){
+                refetch()
+                Swal.fire({
+                    position: "top-center",
+                    icon: "success",
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+        })
+    }
+
+    const handelDeleted = (user)=>{
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              axiosSecure.delete(`/users/${user._id}`)
+              .then(res=>{
+                console.log(res.data);
+                if(res.data.deletedCount> 0){
+                  refetch();
+                   Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+               });
+                }
+              })
+            }
+          });
+    }
     return (
         <div>
             <div  className="flex justify-evenly my-5"> 
@@ -23,19 +73,25 @@ const AllUser = () => {
       <tr>
         <th></th>
         <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
+        <th>Email</th>
+        <th>Role</th>
+        <th>Action</th>
       </tr>
     </thead>
     <tbody>
       {
-        users.map(user=> <tr
+        users.map((user,index)=> <tr
          key={user._id}
         >
-            <th>1</th>
-            <td>Cy Ganderton</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
+            <th>{index+1}</th>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>
+           { user.role  === 'admin'? 'Admin' : <button onClick={()=>handelMakeAdmin(user)} className="btn  bg-orange-500"> <FaUsers className="text-white "></FaUsers> </button>}
+            </td>
+            <td>
+            <button onClick={()=>handelDeleted(user)} className="btn btn-ghost btn-lg"> <FaTrash className="text-red-600 "></FaTrash> </button>
+            </td>
           </tr>)
       }
     
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from "../Firebase/firebase.config";
+import useAxiosPublic from "../Hooks/useAxiosPublic";
 
 
 export const AuthContext = createContext(null)
@@ -13,6 +14,8 @@ const AuthProvider = ({children}) => {
     const [loading, setLoading]= useState(true)
 
     const Googleprovider = new GoogleAuthProvider();
+
+    const axiosPublic = useAxiosPublic()
     
     const createUser = (email, password)=>{
         setLoading(true)
@@ -44,6 +47,19 @@ const AuthProvider = ({children}) => {
     useEffect(()=>{
       const unSubscribe=  onAuthStateChanged(auth , currentUser=>{
             setUser(currentUser)
+            if(currentUser){
+              const userInfo ={email: currentUser.email}
+              axiosPublic.post('/jwt', userInfo)
+              .then(res=>{
+                console.log(res.data.token);
+                if(res.data.token){
+                  localStorage.setItem('access-token', res.data.token)
+                }
+              })
+            }
+            else{
+              localStorage.removeItem('access-token')
+            }
             setLoading(false)
         });
         return ()=>{
